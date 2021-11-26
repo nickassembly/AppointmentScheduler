2738431bd976e787586cb50fc4ff32d12b032397
nickguerra <nickg@maliciousgames.com>
2021-11-26 07:15:37 -0600

    Added validation to appointment modal

--- AppointmentScheduler/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduler/wwwroot/js/script-custom-calendar.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
     });
 
     InitializeCalendar();
+
+
 });
 
 var calendar;
@@ -49,61 +51,58 @@ function onCloseModal() {
     $("#appointmentInput").modal("hide");
 }
 
-//function onSubmitForm() {
-//    if (checkValidation()) {
-//        var requestData = {
-//            Id: parseInt($("#id").val()),
-//            Title: $("#title").val(),
-//            Description: $("#description").val(),
-//            StartDate: $("#appointmentDate").val(),
-//            Duration: $("#duration").val(),
-//            DoctorId: $("#doctorId").val(),
-//            PatientId: $("#patientId").val(),
-//        };
+function onSubmitForm() {
+    if (checkValidation()) {
+        var requestData = {
+            Id: parseInt($("#id").val()),
+            Title: $("#title").val(),
+            Description: $("#description").val(),
+            StartDate: $("#appointmentDate").val(),
+            Duration: $("#duration").val(),
+            DoctorId: $("#doctorId").val(),
+            PatientId: $("#patientId").val(),
+        };
 
-//        $.ajax({
-//            url: routeURL + '/api/Appointment/SaveCalendarData',
-//            type: 'POST',
-//            data: JSON.stringify(requestData),
-//            contentType: 'application/json',
-//            success: function (response) {
-//                if (response.status === 1 || response.status === 2) {
-//                    calendar.refetchEvents();
-//                    $.notify(response.message, "success");
-//                    onCloseModal();
-//                }
-//                else {
-//                    $.notify(response.message, "error");
-//                }
-//            },
-//            error: function (xhr) {
-//                $.notify("Error", "error");
-//            }
-//        });
-//    }
-//}
+        $.ajax({
+            url: routeURL + '/api/Appointment/SaveCalendarData',
+            type: 'POST',
+            data: JSON.stringify(requestData),
+            contentType: 'application/json',
+            success: function (response) {
+                if (response.status === 1 || response.status === 2) {
+                    $.notify(response.message, "success");
+                    onCloseModal();
+                } else {
+                    $.notify(response.message, "error");
+                }
+            },
+            error: function (xhr) {
+                $.notify("Error", "error");
+            }
+        });
+    }
+}
 
-//function checkValidation() {
-//    var isValid = true;
-//    if ($("#title").val() === undefined || $("#title").val() === "") {
-//        isValid = false;
-//        $("#title").addClass('error');
-//    }
-//    else {
-//        $("#title").removeClass('error');
-//    }
+function checkValidation() {
+    var isValid = true;
+    if ($("#title").val() === undefined || $("#title").val() === "") {
+        isValid = false;
+        $("#title").addClass('error');
+    }
+    else {
+        $("#title").removeClass('error');
+    }
 
-//    if ($("#appointmentDate").val() === undefined || $("#appointmentDate").val() === "") {
-//        isValid = false;
-//        $("#appointmentDate").addClass('error');
-//    }
-//    else {
-//        $("#appointmentDate").removeClass('error');
-//    }
+    if ($("#appointmentDate").val() === undefined || $("#appointmentDate").val() === "") {
+        isValid = false;
+        $("#appointmentDate").addClass('error');
+    }
+    else {
+        $("#appointmentDate").removeClass('error');
+    }
 
-//    return isValid;
-
-//}
+    return isValid;
+}
 
 //function getEventDetailsByEventId(info) {
 //    $.ajax({
