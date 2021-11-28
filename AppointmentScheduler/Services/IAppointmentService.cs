b61290620a0316efaf7aebeda08d73ce932a6c92
nickguerra <nickg@maliciousgames.com>
2021-11-28 07:31:38 -0600

    Added method to display appointments

--- AppointmentScheduler/Services/IAppointmentService.cs
+++ AppointmentScheduler/Services/IAppointmentService.cs
@@ -14,5 +14,6 @@ namespace AppointmentScheduler.Services
         public Task<int> AddUpdate(AppointmentVM model);
         public List<AppointmentVM> DoctorsEventsById(string doctorId);
         public List<AppointmentVM> PatientsEventsById(string patientId);
+        public AppointmentVM GetById(int id);
     }
 }
